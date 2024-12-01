2c0835b102b557c3136004a46ba22a421b9ded04
Justin Pyron <justinpyron@gmail.com>
2024-11-30 21:49:58 -0600

    Functional trainer

--- trainer.py
+++ trainer.py
@@ -81,57 +81,73 @@ class Trainer:
         loss.backward()
         self.optimizer.step()
         self.scheduler.step()
+        self.batches_trained_on += 1
 
     def train_one_epoch(self) -> None:
+        self.model.train()
         for i, batch in enumerate(self.dataloader_train):
             self.train_one_batch(*batch)
             if i > 10:  # TODO: delete after testing
                 break
 
-    # def evaluate(
-    #     self,
-    #     num_batches_evaluate : int,
-    #     from_val : bool = True,
-    # ) -> torch.tensor:
-    #     """Compute average loss on a handful of batches"""
-    #     self.model.eval()
-    #     loss_samples = list()
-    #     for i in range(num_batches_evaluate):
-    #         x, y = self.dataloader.get_batch(from_val=from_val)
-    #         with torch.no_grad():
-    #             loss = self.model(x, y)
-    #             loss_samples.append(loss)
-    #     self.model.train()
-    #     return torch.tensor(loss_samples).mean().item()
+    def evaluate_one_batch(
+        self,
+        text_source: list[str],
+        text_destination: list[str],
+    ) -> float:
+        tokens_source, tokens_destination = self.prepare_batch(
+            text_source, text_destination
+        )
+        with torch.no_grad():
+            loss = self.model(
+                tokens_source=tokens_source,
+                tokens_destination=tokens_destination[:, :-1],
+                targets=tokens_destination[:, 1:],
+            )
+            return loss
 
-    # def launch_train_session(
-    #     self,
-    #     num_batches : int,
-    #     evaluate_every : int,
-    #     save_model: bool = True,
-    #     verbose : bool = True,
-    # ):
-    #     self.model.train()
-    #     best_loss = torch.inf
-    #     stopwatch = list()
-    #     for i in range(num_batches):
-    #         start = time.time()
-    #         self.train_on_one_batch()
-    #         stopwatch.append(time.time() - start)
-    #         if i % evaluate_every == 0:
-    #             loss = self.evaluate(self.num_batches_evaluate)
-    #             self.loss_curve.append((self.batches_trained_on, loss))
-    #             if loss < best_loss and save_model:
-    #                 self.save()
-    #                 best_loss = loss
-    #             if verbose:
-    #                 print(" | ".join([
-    #                     f"Batch {self.batches_trained_on:5}",
-    #                     f"Loss = {loss:6.3f}",
-    #                     f"Stopwatch = {sum(stopwatch)/60:5.1f} min ({sum(stopwatch) / len(stopwatch):4.2f} s/batch)",
-    #                     f"lr = {self.scheduler._last_lr[0]:.2E}",
-    #                 ]))
-    #         self.batches_trained_on += 1
+    def evaluate_one_epoch(self) -> None:
+        self.model.eval()
+        loss_list = list()
+        for i, batch in enumerate(self.dataloader_val):
+            loss = self.evaluate_one_batch(*batch)
+            loss_list.append(loss)
+            if i > 10:  # TODO: delete after testing
+                break
+        self.loss_curve.append(
+            (self.batches_trained_on, torch.tensor(loss_list).mean().item())
+        )
+
+    def launch_train_session(
+        self,
+        train_epochs: int,
+        save_model: bool = True,
+        verbose: bool = True,
+    ):
+        best_loss = torch.inf
+        stopwatch = list()
+        for i in range(train_epochs):
+            start = time.time()
+            print("Train epoch...")
+            self.train_one_epoch()
+            print("Evaluation epoch...")
+            self.evaluate_one_epoch()
+            stopwatch.append(time.time() - start)
+            loss = self.loss_curve[-1][1]
+            if loss < best_loss and save_model:
+                self.save()
+                best_loss = loss
+            if verbose:
+                print(
+                    " | ".join(
+                        [
+                            f"Batches {self.batches_trained_on:5}",
+                            f"Loss = {loss:6.3f}",
+                            f"Stopwatch = {sum(stopwatch)/60:5.1f} min ({sum(stopwatch) / len(stopwatch):4.2f} s/batch)",
+                            f"lr = {self.scheduler._last_lr[0]:.2E}",
+                        ]
+                    )
+                )
 
     def save(self):
         torch.save(
